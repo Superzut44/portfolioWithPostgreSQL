caf0e5e8ebc3ffc6b2a4d3bf639d898cba11e00d
chavaudrey <chavaudreyxav@hotmail.fr>
2022-04-03 21:48:39 +0200

    api create hard in controller and sent in json

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -8,11 +8,49 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    public function index(): Response
+    #[Route('/api/projects/test', name: 'api_projects_test', methods: 'GET')]
+    public function getProjects(): Response
     {
-        return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
-        ]);
+        $projects = [
+            [
+                'id' => 1,
+                'name' => 'Olususi Oluyemi',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Camila Terry',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/men/42.jpg'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Joel Williamson',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/67.jpg'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Deann Payne',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+            ],
+            [
+                'id' => 5,
+                'name' => 'Donald Perkins',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/men/89.jpg'
+            ]
+        ];
+    
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setContent(json_encode($projects));
+        
+        return $response;
     }
 }
