c80b91a104d92bad37ea3f32b4209fd81cf2a4b9
chavaudrey <chavaudreyxav@hotmail.fr>
2022-04-24 11:38:43 +0200

    composer validator and assert contraint for Project Entity

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
 class Project
@@ -21,10 +22,11 @@ class Project
     private $name;
 
     #[ORM\Column(type: 'text', nullable: true)]
+    #[Assert\NotBlank]
     #[Groups("project:read")]
     private $description;
 
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[ORM\Column(type: 'string', nullable: true)]
     #[Groups("project:read")]
     private $image;
 
